e98c5aa64bac726d594539a9a885e677221116d2
Gustavo schwanz viegas <55723479+GustavoViegas8@users.noreply.github.com>
2020-09-28 10:47:29 -0300

    Update URI 1003.js

--- URI 1003.js
+++ URI 1003.js
@@ -1,6 +1,7 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-let area = Math.pow(lines.shift(), 2)
-let area2 = 3.14159 * area
-console.log(`A=${area2.toFixed(4)}`)
+let valor1 = parseInt(lines.shift())
+let valor2 = parseInt(lines.shift())
+
+console.log(`SOMA = ${valor1+valor2}`)
